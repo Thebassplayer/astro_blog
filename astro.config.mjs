d96f9d9fe766b359798ac4df6f3298a87063d113
Roy Lopez <roylopezdev@gmail.com>
2023-12-24 16:16:19 -0300

    Add dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://inspiring-hummingbird-145527.netlify.app/"
+  site: "https://inspiring-hummingbird-145527.netlify.app/",
+  integrations: [preact()]
 });
